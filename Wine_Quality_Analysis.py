315f7b41b03c8395bf62f4075d6c7d09db6f5041
flissd <43940543+flissd@users.noreply.github.com>
2020-12-01 11:28:18 -0600

    Update Wine_Quality_Analysis.py

--- Wine_Quality_Analysis.py
+++ Wine_Quality_Analysis.py
@@ -64,6 +64,7 @@ indices = np.array([[0,99],[100,199],[200,299],[300,399],[400,499],[500,599],[60
            [1100,1199],[1200,1299],[1300,1399],[1400,1499],[1500,1598]])
 num_i = len(indices)
 
+
 sq_err = 0
 for index in range(num_i):
     start = indices[index,0]
@@ -95,13 +96,14 @@ for index in range(num_i):
     #record average squared error
     sq_err += sum(pow(x_test@w-y_test,2))/len(y_test)
 sq_err = sq_err/num_i
-
+print("Squared Error: ",sq_err)
+print("Weights: \n",w)
 #SVD the data
 U,s,VT = np.linalg.svd(rw, full_matrices=False)
 #print(s)
 #find the condition number
 cn = s[0]/s[1]
-print(cn)
+print("Condition Number: ",cn)
 
 #kmeans algorithm
 k_val = 1
@@ -109,7 +111,7 @@ k_error_last = 0
 k_error = -1
 feature_k = np.zeros(len(rw))
 
-while ((k_error_last-k_error) > 0.01*k_error_last) or k_error < 0 or k_error_last < 0:
+while ((k_error_last-k_error) > 0.1) or k_error < 0 or k_error_last < 0:
     k_error_last = k_error
     feature_k,k_error = kmeans_algorithm(rw,k_val)
     #print(k_val)
@@ -117,8 +119,52 @@ while ((k_error_last-k_error) > 0.01*k_error_last) or k_error < 0 or k_error_las
     #print(k_error_last)
     #print(k_error)
 
-print(k_val)
-print(feature_k)
+#print(k_val)
+#print(feature_k)
 
 #redo the linear regression with addition group number
-#find the new condition number (if it changes)
+rw_add = np.column_stack((rw,feature_k.reshape(len(feature_k),1)))
+
+sq_err = 0
+for index in range(num_i):
+    start = indices[index,0]
+    end = indices[(index+1)%(num_i),1]
+    if len(rw[:start]) == 0:
+        x_train = rw_add[end+1:]
+        y_train = rd[end+1:]
+        x_test = rw_add[:end+1]
+        y_test = rd[:end+1]
+    elif len(rw[end:]) == 0:
+        x_train = rw_add[:start]
+        y_train = rd[:start]
+        x_test = rw_add[start:]
+        y_test = rd[start:]
+    elif start > end+1:
+        x_train = rw_add[end+1:start]
+        y_train = rd[end+1:start]
+        x_test = np.vstack((rw_add[start:],rw_add[:end+1]))
+        y_test = np.vstack((rd[start:],rd[:end+1]))
+    else:
+        x_train = np.vstack((rw_add[:start],rw_add[end+1:]))
+        y_train = np.vstack((rd[:start],rd[end+1:]))
+        x_test = rw_add[start:end+1]
+        y_test = rd[start:end+1]
+
+    #use linear regression to find model
+    w_new = np.linalg.inv(np.transpose(x_train)@x_train)@np.transpose(x_train)@y_train
+
+    #record average squared error
+    sq_err += sum(pow(x_test@w_new-y_test,2))/len(y_test)
+sq_err = sq_err/num_i
+
+print("New Squared Error: ",sq_err)
+print("New Weights: \n",w_new)
+
+#SVD the data
+U,s,VT = np.linalg.svd(rw_add, full_matrices=False)
+#print(s)
+#find the condition number
+cn = s[0]/s[1]
+print("New Condition Number: ",cn)
+
+#Try again using regularization
